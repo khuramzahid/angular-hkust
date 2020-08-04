fb94b598c0846b4923fcb3a8707abe977a6f55f6
Khuram Zahid <khuramzahid@gmail.com>
2020-08-04 02:50:15 -0700

    In line template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { title } from 'process';
+import { table } from 'console';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<mat-toolbar color="primary"> <span>{{title}}</span> </mat-toolbar>',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'conFusion';
 }
+
+/*
+Component is the decorator applied to the class. 
+It controls the selector through which we can refer to a component in the HTML document.
+It also specifies templateUrl that is rendered when the component is to be mounted.
+We can also have 'template' property instead of 'templateUrl' if the HTML code is small.
+*/
