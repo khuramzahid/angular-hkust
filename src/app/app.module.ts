b4ca2b4251b3e75be889aca1b6419e23a1830609
Muhammad Asad Riaz <m_asad.riaz@hotmail.com>
2020-08-05 15:43:25 +0500

    Header and Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,13 +14,17 @@ import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component'; // automatically added
 import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 // NgModule is a decorator for the class AppModule
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
