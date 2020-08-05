d8d1741b1f919e2c1e7a6be7afcac45cd2878ea9
Khuram Zahid <khuramzahid@gmail.com>
2020-08-05 23:58:23 +0500

    Single Page Applications Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,14 +13,14 @@ import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component'; // automatically added
-import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
-
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 // NgModule is a decorator for the class AppModule
 @NgModule({
   declarations: [
@@ -44,7 +44,10 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FlexLayoutModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [
+    DishService, 
+    PromotionService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
