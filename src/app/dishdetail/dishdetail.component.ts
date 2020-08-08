dba4247ab2a303518b0c2a89449676142d377eaf
Khuram Zahid <khuramzahid@gmail.com>
2020-08-08 20:05:30 +0500

    RxJS with SwitchMap

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
+import { switchMap } from 'rxjs/operators';
 
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
@@ -13,20 +14,34 @@ import { Location } from '@angular/common';
 export class DishdetailComponent implements OnInit {
 
   dish: Dish;
+  dishIds: string[];
+  prev: string;
+  next: string;
 
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
     private location: Location) { }
 
   ngOnInit() {
-    let id = this.route.snapshot.params['id'];
-    this.dishservice.getDish(id)
-      .subscribe(dish => this.dish = dish);
+    this.dishservice.getDishIds()
+      .subscribe(dishIds => this.dishIds = dishIds);
+    this.route.params
+      .pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
+      .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
     // method chaining is achieved when all functions use "return this;" at the end.
+    // switchMap is used here to replace the params observable value with the value from another observable getDish
+    // params is an array which means it can be taken as a Read stream. We can pipe read streams.
+    // pipe collects streamed bits and dispatches the data when a chunk is formed, into the destination specified in its arguments.
   }
 
   goBack(): void {
     this.location.back();
+  }
+
+  setPrevNext(dishId: string) {
+    const index = this.dishIds.indexOf(dishId);
+    this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
+    this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
   }
 
 }
