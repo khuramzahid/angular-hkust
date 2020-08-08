61d0c2f3f3d8649a79dda96aa38ca7250f2c8889
Khuram Zahid <khuramzahid@gmail.com>
2020-08-08 13:43:30 +0500

    Reactive Forms 1

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Feedback, ContactType } from '../shared/feedback';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  feedbackForm: FormGroup; // Form model that will host the reactive form
+  feedback: Feedback; // data model for holding states
+  contactType = ContactType;
+
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
 
   ngOnInit() {
+  }
+
+  createForm() {
+    this.feedbackForm = this.fb.group({
+      firstname: '',
+      lastname: '',
+      telnum: 0,
+      email: '',
+      agree: false,
+      contacttype: 'None',
+      message: ''
+    }); // this.fb.group() returns an instance of type FormGroup. Note the object passed in the param. It doesn't have to match the data model fully.
+  }
+
+  onSubmit() {
+    this.feedback = this.feedbackForm.value; // directly equating only because coincidently the structures match.
+    console.log(this.feedback);
+    this.feedbackForm.reset();
   }
 
 }
