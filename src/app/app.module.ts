8b02114c73e6c6de6e44e60850b4ee25b754e8f9
Khuram Zahid <khuramzahid@gmail.com>
2020-08-09 02:55:19 +0500

    Assignment 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { FlexLayoutModule } from '@angular/flex-layout';
-
-import { AppComponent } from './app.component';
-
-import 'hammerjs';
-import { MenuComponent } from './menu/menu.component';
-import { DishdetailComponent } from './dishdetail/dishdetail.component'; // automatically added
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { AboutComponent } from './about/about.component';
-import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
-import { AppRoutingModule } from './app-routing/app-routing.module';
-import { DishService } from './services/dish.service';
-import { PromotionService } from './services/promotion.service';
-import { LeaderService } from './services/leader.service';
-import { LoginComponent } from './login/login.component';
 import { MatDialogModule } from '@angular/material/dialog';
+import { AppComponent } from './app.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { FormsModule } from '@angular/forms'; 
+import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSliderModule } from '@angular/material/slider';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component';
 
-// NgModule is a decorator for the class AppModule
 @NgModule({
   declarations: [
     AppComponent,
@@ -41,40 +40,41 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     DishdetailComponent,
     HeaderComponent,
     FooterComponent,
-    AboutComponent,
     HomeComponent,
+    AboutComponent,
     ContactComponent,
     LoginComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
     MatToolbarModule,
+    MatListModule,
     MatGridListModule,
+    MatSelectModule,
+    MatSlideToggleModule,
     MatCardModule,
     MatButtonModule,
-    MatListModule,
-    FlexLayoutModule,
-    AppRoutingModule,
-    MatDialogModule,
     MatFormFieldModule, 
     MatInputModule,
     MatCheckboxModule,
+    MatDialogModule,
     FormsModule,
-    MatSelectModule,
-    MatSlideToggleModule,
+    ReactiveFormsModule,
     MatProgressSpinnerModule,
-    ReactiveFormsModule
-  ],
-  providers: [
-    DishService, 
-    PromotionService,
-    LeaderService
+    MatSliderModule,
+    HttpClientModule
   ],
   entryComponents: [
     LoginComponent
   ],
+  providers: [
+    DishService,
+    PromotionService,
+    LeaderService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-// To make this component be opened from another component, you need to declare this as an EntryComponent in the AppModule.
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { switchMap } from 'rxjs/operators';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Comment } from '../shared/comment';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
@@ -14,13 +15,88 @@ import { Location } from '@angular/common';
 export class DishdetailComponent implements OnInit {
 
   dish: Dish;
+  dishcopy: Dish;
+  errMess: string;
   dishIds: string[];
   prev: string;
   next: string;
+  commentForm: FormGroup;
+  commentDS: Comment;
+  BaseURL: string;
+  visibility = 'shown';
+
+  formErrors = {
+    'author': '',
+    'comment': ''
+  };
+
+  validationMessages = {
+    'author': {
+      'required':      'Author is required.',
+      'minlength':     'Author must be at least 2 characters long.',
+    },
+    'comment': {
+      'required':      'Comment is required.'
+    }
+  };
+
+  @ViewChild('cform') commentFormDirective; // to reset the form completely
 
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location,
+    private fb: FormBuilder) {
+      this.createForm();
+    }
+  
+  createForm() {
+    this.commentForm = this.fb.group({
+      rating: 5,
+      comment: ['', Validators.required ],
+      author: ['', [Validators.required, Validators.minLength(2)] ],
+      date: ''
+    });
+
+    this.commentForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));    // valueChanges is a prebuilt observable service
+
+    this.onValueChanged(); // (re)set validation messages now
+  } 
+
+  onValueChanged(data?: any) {
+    if (!this.commentForm) { return; }
+    const form = this.commentForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        // clear previous error message (if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
+  }
+
+  onSubmit() {
+    this.commentDS = this.commentForm.value;
+    this.commentDS.date = new Date().toISOString()
+    console.log(this.commentDS);
+    this.dish.comments.push(this.commentDS);
+
+    this.commentForm.reset({
+      rating: 5,
+      comment: '',
+      author: '',
+      date: ''
+    });
+    this.commentFormDirective.resetForm(); // to reset the form completely
+  }
 
   ngOnInit() {
     this.dishservice.getDishIds()
