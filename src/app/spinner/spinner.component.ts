3a38b61b68655748e307f2449c64910ae1e0c9d6
Khuram Zahid <khuramzahid@gmail.com>
2020-08-12 15:59:55 +0500

    Loading Spinner as a nested component

--- src/app/spinner/spinner.component.ts
+++ src/app/spinner/spinner.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-spinner',
-  template: '<div><mat-spinner></mat-spinner><h4>{{textToDisplay}}</h4></div>',
+  template: `<div><mat-spinner></mat-spinner><h4>{{textToDisplay}}</h4></div>`,
   styleUrls: ['./spinner.component.scss']
 })
 export class SpinnerComponent {
